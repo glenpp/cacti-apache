737ee52abbb96abbde68ef28bbbd662695d3952f
Glen Pitt-Pladdy <gpp-hg@pitt-pladdy.com>
2012-11-15 19:17:29 +0000

    Shell version created

---
+++ apache-stats.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# Copyright (C) 2012  Glen Pitt-Pladdy
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#
+#
+
+CACHETIME=30
+CACHEFILE=/var/local/snmp/cache/apache
+CACHEFILE=/tmp/t1
+
+# check for cache file newer CACHETIME seconds ago
+if [ ! -f $CACHEFILE -o $((`date +%s`-`stat --format=%Y $CACHEFILE`)) -ge $CACHETIME ]; then
+	# update the data
+	wget --output-document=$CACHEFILE.TMP.$$ 'http://localhost/server-status?auto' >/dev/null 2>&1
+	mv $CACHEFILE.TMP.$$ $CACHEFILE
+fi
+
+# output the data in order (this is because some platforms don't have them all)
+for field in \
+	'Total Accesses' \
+	'Total kBytes' \
+	'CPULoad' \
+	'Uptime' \
+	'ReqPerSec' \
+	'BytesPerSec' \
+	'BytesPerReq' \
+	'BusyWorkers' \
+	'IdleWorkers'
+do
+	if [ "$field" = 'Total kBytes' ]; then
+		echo $((`grep "^$field:" <$CACHEFILE | sed "s/^$field: *//"`*1024))
+	else
+		grep "^$field:" <$CACHEFILE | sed "s/^$field: *//"
+	fi
+done
+
+# count up the scorecard
+scorecard=`grep ^Scoreboard: $CACHEFILE | sed 's/^Scoreboard: *//'`
+echo $scorecard | sed 's/[^_]//g' | wc -c
+echo $scorecard | sed 's/[^S]//g' | wc -c
+echo $scorecard | sed 's/[^R]//g' | wc -c
+echo $scorecard | sed 's/[^W]//g' | wc -c
+echo $scorecard | sed 's/[^K]//g' | wc -c
+echo $scorecard | sed 's/[^D]//g' | wc -c
+echo $scorecard | sed 's/[^C]//g' | wc -c
+echo $scorecard | sed 's/[^L]//g' | wc -c
+echo $scorecard | sed 's/[^G]//g' | wc -c
+echo $scorecard | sed 's/[^I]//g' | wc -c
+echo $scorecard | sed 's/[^\.]//g' | wc -c
+
+
+
